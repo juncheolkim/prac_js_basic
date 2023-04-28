33e7f183769827db4f585829533d0b67dc168daf
juncheolkim <kimjoon889@gmail.com>
2023-04-28 19:00:00 +0900

    문자열과 연산자 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#Escape_notation 참고

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -3,6 +3,8 @@ let currentResult = defaultResult; // camelCase
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1; // operations
 
-let calculationDesciption = "(" + defaultResult + "+ 10) * 3 / 2 - 1";
+let calculationDesciption = `( ${defaultResult}+ 10 ) * 3 / 2 - 1`;
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#Escape_notation
+// let errorMessage = "An error \n occurred!";
 
 outputResult(currentResult, calculationDesciption);
